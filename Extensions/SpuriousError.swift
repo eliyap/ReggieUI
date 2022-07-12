336aa8bb1f89a83bdd430ecb40ae05d7b9fbcb4f
eliyap <yap@usc.edu>
2022-07-12 07:48:37 -0400

    add a testing error
    so I can respond to `try Realm()` failure

---
+++ Extensions/SpuriousError.swift
@@ -0,0 +1,13 @@
+//
+//  SpuriousError.swift
+//  ReggieUI
+//
+//  Created by Secret Asian Man Dev on 12/7/22.
+//
+
+import Foundation
+
+/// An error which can be thrown in development to test error handling.
+internal enum SpuriousError: Error {
+    case problem
+}
