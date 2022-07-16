174676452f06de99df818ece9c3c9e4a7b09365d
eliyap <yap@usc.edu>
2022-07-16 09:45:52 -0400

    create app intent
    action appears in shortcuts, very easy

---
+++ RexIntents/RexIntents.swift
@@ -0,0 +1,18 @@
+//
+//  RexIntents.swift
+//  RexIntents
+//
+//  Created by Secret Asian Man Dev on 16/7/22.
+//
+
+import AppIntents
+
+public struct RexIntent: AppIntent {
+    public static var title: LocalizedStringResource = "RexIntents"
+
+    public init() { }
+
+    public func perform() async throws -> some IntentResult {
+        return .result(value: "Hello World")
+    }
+}
---
+++ RexIntents/RexIntentsExtension.swift
@@ -0,0 +1,12 @@
+//
+//  RexIntentsExtension.swift
+//  RexIntents
+//
+//  Created by Secret Asian Man Dev on 16/7/22.
+//
+
+import AppIntents
+
+@main
+struct RexIntentsExtension: AppIntentsExtension {
+}
