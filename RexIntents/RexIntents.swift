43f765259a8c6b926ea145b1cb0721f509b22a89
eliyap <yap@usc.edu>
2022-07-16 12:21:04 -0400

    draft some simple actions

--- RexIntents/RexIntents.swift
+++ RexIntents/RexIntents.swift
@@ -8,12 +8,66 @@
 import AppIntents
 import RealmSwift
 
-public struct RexIntent: AppIntent {
-    public static var title: LocalizedStringResource = "RexIntents"
+public struct WholeMatchIntent: AppIntent {
+    public static var title: LocalizedStringResource = "Match Entire Text"
 
+    @Parameter(title: "Text")
+    var text: String
+    
+    #warning("replace with real pattern")
+    #warning("look into default value argument")
+    @Parameter(title: "Pattern")
+    var pattern: String
+    
     public init() { }
-
+    
+    static public var parameterSummary: some ParameterSummary {
+        Summary("Check if \(\.$text) matches \(\.$pattern)")
+    }
+    
     public func perform() async throws -> some IntentResult {
+        #warning("todo: return app entity matches?")
         return .result(value: "Hello World")
     }
+}
+
+public struct FindMatchesIntent: AppIntent {
+    public static var title: LocalizedStringResource = "Find Matches in Text"
+
+    @Parameter(title: "Number of Matches", default: MatchMode.first)
+    var matchMode: MatchMode
+    
+    #warning("replace with real pattern")
+    #warning("look into default value argument")
+    @Parameter(title: "Pattern")
+    var pattern: String
+    
+    @Parameter(title: "Text")
+    var text: String
+    
+    public init() { }
+    
+    static public var parameterSummary: some ParameterSummary {
+        Summary("Find \(\.$matchMode) of \(\.$pattern) in \(\.$text)")
+    }
+    
+    public func perform() async throws -> some IntentResult {
+        #warning("todo: return app entity matches?")
+        return .result(value: "Hello World")
+    }
+}
+
+public enum MatchMode: String, RawRepresentable {
+    case first
+    case all
+}
+
+extension MatchMode: AppEnum {
+    
+    public static var typeDisplayRepresentation: TypeDisplayRepresentation = "Number of Matches"
+    
+    public static var caseDisplayRepresentations: [MatchMode : DisplayRepresentation] = [
+        .first: "first match",
+        .all: "all matches",
+    ]
 }
